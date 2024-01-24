adcc4ea05c58e6b3de8f52989affd40a961a3a8b
Masaya Kataoka <ms.kataoka@gmail.com>
2024-01-24 21:20:13 +0900

    apply reformat
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- include/vrx_bridge/vrx_bridge_component.hpp
+++ include/vrx_bridge/vrx_bridge_component.hpp
@@ -23,35 +23,25 @@ extern "C" {
 // demos/composition/include/composition/visibility_control.h at https://github.com/ros2/demos
 #if defined _WIN32 || defined __CYGWIN__
 #ifdef __GNUC__
-#define VRX_BRIDGE_COMPONENT_EXPORT \
-  __attribute__((dllexport))
-#define VRX_BRIDGE_COMPONENT_IMPORT \
-  __attribute__((dllimport))
+#define VRX_BRIDGE_COMPONENT_EXPORT __attribute__((dllexport))
+#define VRX_BRIDGE_COMPONENT_IMPORT __attribute__((dllimport))
 #else
-#define VRX_BRIDGE_COMPONENT_EXPORT \
-  __declspec(dllexport)
-#define VRX_BRIDGE_COMPONENT_IMPORT \
-  __declspec(dllimport)
+#define VRX_BRIDGE_COMPONENT_EXPORT __declspec(dllexport)
+#define VRX_BRIDGE_COMPONENT_IMPORT __declspec(dllimport)
 #endif
 #ifdef VRX_BRIDGE_COMPONENT_BUILDING_DLL
-#define VRX_BRIDGE_COMPONENT_PUBLIC \
-  VRX_BRIDGE_COMPONENT_EXPORT
+#define VRX_BRIDGE_COMPONENT_PUBLIC VRX_BRIDGE_COMPONENT_EXPORT
 #else
-#define VRX_BRIDGE_COMPONENT_PUBLIC \
-  VRX_BRIDGE_COMPONENT_IMPORT
+#define VRX_BRIDGE_COMPONENT_PUBLIC VRX_BRIDGE_COMPONENT_IMPORT
 #endif
-#define VRX_BRIDGE_COMPONENT_PUBLIC_TYPE \
-  VRX_BRIDGE_COMPONENT_PUBLIC
+#define VRX_BRIDGE_COMPONENT_PUBLIC_TYPE VRX_BRIDGE_COMPONENT_PUBLIC
 #define VRX_BRIDGE_COMPONENT_LOCAL
 #else
-#define VRX_BRIDGE_COMPONENT_EXPORT \
-  __attribute__((visibility("default")))
+#define VRX_BRIDGE_COMPONENT_EXPORT __attribute__((visibility("default")))
 #define VRX_BRIDGE_COMPONENT_IMPORT
 #if __GNUC__ >= 4
-#define VRX_BRIDGE_COMPONENT_PUBLIC \
-  __attribute__((visibility("default")))
-#define VRX_BRIDGE_COMPONENT_LOCAL \
-  __attribute__((visibility("hidden")))
+#define VRX_BRIDGE_COMPONENT_PUBLIC __attribute__((visibility("default")))
+#define VRX_BRIDGE_COMPONENT_LOCAL __attribute__((visibility("hidden")))
 #else
 #define VRX_BRIDGE_COMPONENT_PUBLIC
 #define VRX_BRIDGE_COMPONENT_LOCAL
@@ -63,11 +53,10 @@ extern "C" {
 #endif
 
 #include <geographic_msgs/msg/geo_pose_stamped.hpp>
-#include <sensor_msgs/msg/imu.hpp>
-#include <sensor_msgs/msg/nav_sat_fix.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <rclcpp/rclcpp.hpp>
-
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/nav_sat_fix.hpp>
 
 namespace vrx_bridge
 {
